681b1fccc102464bd0b0c36542a7e02054180c03
Vipulkumar Mahadik <vipul.mahadik@gmail.com>
2020-02-03 23:01:01 -0800

    Updates

---
+++ precache-manifest.eea9a2137a34fc1aac9926b8284566d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4deb9b100dacc8f215f0fdf6d0a8f429",
+    "url": "/financee-dashboard/index.html"
+  },
+  {
+    "revision": "b596da53c9d802bdf3ec",
+    "url": "/financee-dashboard/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "557add3f30988c75aa03",
+    "url": "/financee-dashboard/static/css/main.6301b4c7.chunk.css"
+  },
+  {
+    "revision": "b596da53c9d802bdf3ec",
+    "url": "/financee-dashboard/static/js/2.21e283d2.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/financee-dashboard/static/js/2.21e283d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "557add3f30988c75aa03",
+    "url": "/financee-dashboard/static/js/main.bd9492ed.chunk.js"
+  },
+  {
+    "revision": "d2fbad6fad6d335bc849",
+    "url": "/financee-dashboard/static/js/runtime-main.e252ed61.js"
+  }
+]);
